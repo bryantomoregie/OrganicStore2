1d0358c1368e7c4d8a11f2acfdc8ee693fa1e3df
bryantomoregie <unclebryant@gmail.com>
2021-03-15 11:28:44 -0500

    mobile view for all categories coming along

---
+++ Pages/Category/Index.cshtml.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using OrganicStore2.Models;
+
+namespace OrganicStore2.Pages.Category
+{
+    public class IndexModel : PageModel
+    {
+        private readonly OrganicStoreDbContext _context;
+
+        public IndexModel(OrganicStoreDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Product> Products { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Products = await _context.Product
+                .Where(x => x.Type == "Produce")
+                .ToListAsync();
+        }
+    }
+}
